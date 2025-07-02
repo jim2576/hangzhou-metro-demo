0f95bc7b7bf7a8530e6a357f9a14899b0c47b84e
jim2576 <2790194628@qq.com>
2025-07-02 22:27:39 +0800

    Delete hangzhou-metro-demo directory

--- hangzhou-metro-demo/part3.js
+++
@@ -1,9 +0,0 @@
-    // 简单的服务卡片交互动效
-    document.addEventListener('DOMContentLoaded', function() {
-        const cards = document.querySelectorAll('.service-card');
-        cards.forEach(card => {
-            card.addEventListener('click', function() {
-                alert('功能模块建设中，感谢关注杭州地铁！');
-            });
-        });
-    });
